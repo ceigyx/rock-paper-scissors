438bfd68131338349b8d4b992fa5e953791a12a2
ceigyx <aklypse_ceigyx@live.ca>
2019-11-21 22:07:53 -0500

    Added wrapping main function, implemented support for automatic player select and number of rounds

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,65 @@
-var options = ['rock', 'paper', 'scissors'];
-var playerPoints = 0;
-var computerPoints = 0;
+function game(ans, numOfRounds) {
 
+    //Tracking and Setup
+    const options = ['rock', 'paper', 'scissors'];
+    var playerPoints = 0;
+    var computerPoints = 0;
+    var roundWin = '';
+    var debug = ans;
+    var rounds = 5;
+    if (numOfRounds) { rounds = numOfRounds};
 
-function computerPlay() {
-    return options[Math.floor(Math.random() * 3)];
-}
+    //Gameplay input and logic functions
+    function computerPlay() {
+        return options[Math.floor(Math.random() * 3)];
+    }
 
-function playerSelection() {
-    let input = prompt('Choose! - Rock or Paper or Scissors?').toLowerCase();
-    while (options.indexOf(input) === -1) {
-    input = prompt('Wrong choice, Try again! - Rock or Paper or Scissors?').toLowerCase();
+    function playerSelection() {
+        if (options.indexOf(debug) !== -1) {
+            return debug;
+        }
+        let input = prompt('Choose! - Rock or Paper or Scissors?').toLowerCase();
+        while (options.indexOf(input) === -1) {
+        input = prompt('Wrong choice, Try again! - Rock or Paper or Scissors?').toLowerCase();
+        }
+        return input;
     }
-    return input;
-}
 
-function playRound() {
-    let player = playerSelection().length;
-    let computer = computerPlay().length;
-    let diff = player - computer;
-    console.log(player + ' vs ' + computer);
-    if (player === computer) {
-        return 'draw';
+    function playRound() {
+        let player = playerSelection();
+        let computer = computerPlay();
+        console.log('player chose: ' + player);
+        console.log('computer chose: ' + computer);
+        let diff = player.length - computer.length;
+        if (diff === 0) {
+            roundWin = 'draw';
+        }
+        else if ((diff === -4) || (diff === 3) ||(diff === 1)) {
+            playerPoints++;
+            roundWin = 'player';
+        }
+        else {
+            computerPoints++;
+            roundWin = 'computer';
+        }
+        return roundWin;
+    } 
+
+    //Main process
+    for (var i = 0; i < rounds; i++)
+    {
+        playRound();
+        console.log('round ' + (i + 1) + ' winner: ' + roundWin);
+        console.log('----------------------------------------------');
     }
-    else if ((diff === -4) || (diff === 3) ||(diff === 1)) {
-        playerPoints++;
-        return 'player';
+
+    //Results
+    console.log('player wins: ' + playerPoints)
+    console.log('computer wins: ' + computerPoints)
+
+    if (playerPoints > computerPoints) {
+        return 'you won the game';
     }
-    else {
-        computerPoints++;
-        return 'computer';
-    }
+    return 'you lost the game';
+
 }
